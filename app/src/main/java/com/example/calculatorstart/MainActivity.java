d3c892b29b10d363b170c7abb87aa7e0869ee238
Василий Дворак <106032465+VasylDvorak@users.noreply.github.com>
2022-10-30 22:40:55 +0300

    begin

--- app/src/main/java/com/example/calculatorstart/MainActivity.java
+++ app/src/main/java/com/example/calculatorstart/MainActivity.java
@@ -15,17 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Txt = findViewById(R.id.text);
-        Txt.setText("Передаём число "+ String.valueOf(arg));
-findViewById(R.id.launch).setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-
-        Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("vasyl://calculator"));
-        intent.putExtra("first_argument", arg);
-        startActivity(intent);
-
-    }
-});
+       
     }
 }
