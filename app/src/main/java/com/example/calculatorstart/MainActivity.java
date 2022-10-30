f31e88ae9fc2ac555abd18a17691cc7adf3793bf
Vasyl <dvorak1981@yandex.ru>
2022-10-30 19:49:09 +0300

    Program to start calculator

---
+++ app/src/main/java/com/example/calculatorstart/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.calculatorstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView Txt;
+    private float arg=3.14159265358f;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Txt = findViewById(R.id.text);
+        Txt.setText("Передаём число "+ String.valueOf(arg));
+findViewById(R.id.launch).setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+
+        Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("vasyl://calculator"));
+        intent.putExtra("first_argument", arg);
+        startActivity(intent);
+
+    }
+});
+    }
+}
